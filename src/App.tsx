0d6471c99e759371eb7d1dddb97f422560436c21
unknown <rossivini1010@gmail.com>
2025-08-14 17:53:03 -0300

    Feat: Adding Style Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // import para dar certo as rotas do site
+import { Home, Leads, Login, Profile, Registration } from "./pages";
 
 // rotas do site sendo router o pai, routes o "irmao mais velho" e route os links em si
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<>LOGIN</>} />
-        <Route path="/cadastro" element={<>CADASTRO</>} />
-        <Route path="/home" element={<>HOME</>} />
-        <Route path="/leads" element={<>LEADS</>} />
-        <Route path="/perfil" element={<>PERFIL</>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Registration />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/leads" element={<Leads />} />
+        <Route path="/perfil" element={<Profile />} />
       </Routes>
     </Router>
   );
